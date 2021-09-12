c392ffeee8092bfbb81f454720625baad71d124e
Marcus Pereira <mpereirassa@gmail.com>
2021-09-12 20:59:49 -0300

    WIP register before_request hook

--- mvc_flask/__init__.py
+++ mvc_flask/__init__.py
@@ -1,16 +1,26 @@
-from importlib import import_module
 import json
-
+from collections import namedtuple
+from importlib import import_module
 from pathlib import Path
+from typing import List
+from .hook import Hook
+
 from flask import Flask
 from flask.blueprints import Blueprint
-from collections import namedtuple
 
-
-Route = namedtuple("Route", ["method", "path", "controller", "action"])
+Route = namedtuple(
+    "Route", ["method", "path", "controller", "action", "hooks"]
+)
 
 
 class FlaskMVC:
+    """FlaskMVC extension mvc-flask
+
+    Usage:
+        mvc = FlaskMVC()
+        mvc.init_app(app)
+    """
+
     def __init__(self, app: Flask = None, directory: str = "app"):
         self.directory = Path(directory)
 
@@ -18,9 +28,16 @@ class FlaskMVC:
             self.init_app(app)
 
     def init_app(self, app):
+        self.hook = Hook()
+
         self.create_blueprint(app)
 
-    def routes(self):
+    def routes(self) -> List[Route]:
+        """Get all routes registered in routes.json
+
+        Returns:
+            List[Route]: Route is a class that contains properties such as: method, path, controller, action and hooks.
+        """
         routes = []
         with open(self.directory / "routes.json", mode="r") as f:
             routes = [
@@ -29,20 +46,26 @@ class FlaskMVC:
                     route["path"],
                     route["controller"],
                     route["action"],
+                    route.get("hooks"),
                 )
                 for route in json.load(f)
             ]
         return routes
 
     def create_blueprint(self, app: Flask):
+        """Create blueprint for register all routes
+
+        Args:
+            app (Flask): instance of current flask application
+        """
         for route in self.routes():
-            dd = (
+            controller_file = (
                 self.directory
                 / "controllers"
                 / f"{route.controller}_controller"
             )
 
-            obj = import_module(dd.as_posix().replace("/", "."))
+            obj = import_module(controller_file.as_posix().replace("/", "."))
             controller = getattr(obj, f"{route.controller.title()}Controller")
 
             blueprint = Blueprint(route.controller, route.controller)
@@ -52,4 +75,9 @@ class FlaskMVC:
                 view_func=getattr(controller(), route.action),
                 methods=[route.method],
             )
+
+            self.hook.register(
+                route.hooks, blueprint=blueprint, controller=controller
+            )
+
             app.register_blueprint(blueprint)
--- sample_app/controllers/home_controller.py
+++ sample_app/controllers/home_controller.py
@@ -4,3 +4,6 @@ from flask import render_template
 class HomeController:
     def index(self):
         return render_template("index.html")
+
+    def aftermoon(self):
+        print("good aftermoon!")
