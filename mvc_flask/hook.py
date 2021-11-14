e66f45d3575a17f6097b69d78222a41d8194d304
Marcus Pereira <mpereirassa@gmail.com>
2021-11-14 18:57:21 -0300

    update

--- mvc_flask/hook.py
+++
@@ -1,8 +0,0 @@
-class Hook:
-    def register(self, hooks, **kwargs):
-        if hooks:
-            for hook in hooks.items():
-                if hook[0] == "before_request":
-                    kwargs["blueprint"].before_request(
-                        getattr(kwargs["controller"](), hook[1])
-                    )
