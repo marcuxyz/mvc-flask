a1caee87ce639536f482e4d3e5eb65ffa587c10b
taconi <igor.taconi@protonmail.com>
2022-10-17 14:11:19 -0300

    Add namespace (#25)

---
+++ tests/app/controllers/posts_controller.py
@@ -0,0 +1,15 @@
+class PostsController:
+    def index(self):
+        return {}, 200
+
+    def create(self):
+        return {}, 201
+
+    def show(self, id):
+        return {}, 200
+
+    def update(self, id):
+        return {}, 202
+
+    def delete(self, id):
+        return {}, 202
--- tests/routes_test.py
+++ tests/routes_test.py
@@ -1,42 +1,58 @@
 from collections import Counter
 
-from ward import test
+from ward import each, test
 
 from tests.fixtures import client
 
 
-@test("confirm if messages blueprint was registered", tags=["routes"])
-def _(client=client):
-    assert "messages" in client.application.blueprints
+@test("confirm if blueprints was registered", tags=["routes"])
+def _(client=client, resource=each("messages", "health", "user", "posts")):
+    assert resource in client.application.blueprints
 
 
 @test("verify if exists duplicate blueprints registered", tags=["routes"])
 def _(client=client):
     assert Counter(client.application.blueprints.keys()) == {
-        "messages": 1,
+        "messages": 1, "health": 1, "user": 1, "posts": 1
     }
 
 
 @test("and view registered path", tags=["routes"])
-def _(client=client):
+def _(
+    client=client,
+    endpoint=each(
+        "/messages",
+        "/messages/new",
+        "/messages/<id>",
+        "/messages/<id>/edit",
+        "/api/v1/health",
+        "/api/v1/posts",
+        "/api/v1/posts/<id>",
+        "/api/v1/user",
+        "/api/v1/user/new",
+        "/api/v1/user/<id>",
+        "/api/v1/user/<id>/edit",
+
+    ),
+):
     routes = [route.rule for route in client.application.url_map.iter_rules()]
 
-    assert "/messages" in routes
+    assert endpoint in routes
 
 
 @test("and view registered endpoints", tags=["routes"])
-def _(client=client):
+def _(client=client, resource=each("messages", "user")):
     endpoints = [
         route.endpoint for route in client.application.url_map.iter_rules()
     ]
 
-    assert "messages.index" in endpoints
-    assert "messages.show" in endpoints
-    assert "messages.new" in endpoints
-    assert "messages.create" in endpoints
-    assert "messages.edit" in endpoints
-    assert "messages.update" in endpoints
-    assert "messages.delete" in endpoints
+    assert f"{resource}.index" in endpoints
+    assert f"{resource}.show" in endpoints
+    assert f"{resource}.new" in endpoints
+    assert f"{resource}.create" in endpoints
+    assert f"{resource}.edit" in endpoints
+    assert f"{resource}.update" in endpoints
+    assert f"{resource}.delete" in endpoints
 
 
 @test("and count verbs registered for HTTP", tags=["routes"])
@@ -47,8 +63,8 @@ def _(client=client):
         for route in routes.methods
     ]
 
-    assert methods.count("GET") == 5
-    assert methods.count("POST") == 1
-    assert methods.count("PUT") == 1
-    assert methods.count("PATCH") == 1
-    assert methods.count("DELETE") == 1
+    assert methods.count("GET") == 13
+    assert methods.count("POST") == 3
+    assert methods.count("PUT") == 3
+    assert methods.count("PATCH") == 3
+    assert methods.count("DELETE") == 2
