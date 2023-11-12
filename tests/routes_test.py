f1d49bd6e574ecd9dfb0344dc71adae4ae4695e9
Marcus Pereira <mpereirassa@gmail.com>
2023-11-12 13:47:58 -0300

    test(routes_test.py): refactor test methods to improve readability and maintainability test(routes_test.py): add assertions to verify that the expected blueprints have been registered test(routes_test.py): add assertions to verify that the expected routes have been registered test(routes_test.py): add assertions to verify that the expected endpoints have been registered

--- tests/routes_test.py
+++ tests/routes_test.py
@@ -1,72 +1,35 @@
-from unittest import TestCase
 from collections import Counter
 
 from tests.app import create_app
 
 
-class TestStringMethods(TestCase):
-    def setUp(self) -> None:
-        app = create_app()
-        app.testing = True
-        self.client = app.test_client()
+def test_when_blueprints_have_been_registered(client):
+    assert set(client.application.blueprints) == {"messages", "health", "user", "posts"}
 
-    def test_when_blueprints_have_been_registered(self):
-        expected_blueprints = {"messages", "health", "user", "posts"}
 
-        self.assertEqual(set(self.blueprints), expected_blueprints)
+def test_when_not_exists_registered_blueprints(client):
+    assert Counter(client.application.blueprints.keys()) == {
+        "messages": 1,
+        "health": 1,
+        "user": 1,
+        "posts": 1,
+    }
 
-    def test_when_not_exists_registered_blueprints(self):
-        self.assertEqual(
-            Counter(self.blueprints.keys()),
-            {"messages": 1, "health": 1, "user": 1, "posts": 1},
-        )
+def test_when_messages_routes_have_been_registered(client):
+    endpoints = ["/messages", "/messages/new", "/messages/<id>", "/messages/<id>/edit"]
+    routes = [route.rule for route in client.application.url_map.iter_rules()]
 
-    def test_when_routes_have_been_registered(self):
-        endpoints = [
-            "/messages",
-            "/messages/new",
-            "/messages/<id>",
-            "/messages/<id>/edit",
-            "/api/v1/health",
-            "/api/v1/posts",
-            "/api/v1/posts/<id>",
-            "/api/v1/user",
-            "/api/v1/user/new",
-            "/api/v1/user/<id>",
-            "/api/v1/user/<id>/edit",
-        ]
-        routes = [route.rule for route in self.client.application.url_map.iter_rules()]
+    for endpoint in endpoints:
+        assert endpoint in routes
 
-        for endpoint in endpoints:
-            self.assertIn(endpoint, routes)
 
-    def test_when_endpoints_have_been_registered(self):
-        for resource in ["messages", "user"]:
-            self.assertIn(f"{resource}.index", self.endpoints)
-            self.assertIn(f"{resource}.show", self.endpoints)
-            self.assertIn(f"{resource}.new", self.endpoints)
-            self.assertIn(f"{resource}.create", self.endpoints)
-            self.assertIn(f"{resource}.edit", self.endpoints)
-            self.assertIn(f"{resource}.update", self.endpoints)
-            self.assertIn(f"{resource}.delete", self.endpoints)
+def test_when_messages_endpoint_have_been_registered(client):
+    endpoints = [route.endpoint for route in client.application.url_map.iter_rules()]
 
-    def test_when_when_there_are_many_registered_routes(self):
-        methods = [
-            route
-            for routes in self.client.application.url_map.iter_rules()
-            for route in routes.methods
-        ]
-
-        self.assertEqual(methods.count("GET"), 13)
-        self.assertEqual(methods.count("POST"), 3)
-        self.assertEqual(methods.count("PUT"), 3)
-        self.assertEqual(methods.count("PATCH"), 3)
-        self.assertEqual(methods.count("DELETE"), 2)
-
-    @property
-    def endpoints(self):
-        return [url.endpoint for url in self.client.application.url_map.iter_rules()]
-
-    @property
-    def blueprints(self):
-        return self.client.application.blueprints
+    assert f"messages.index" in endpoints
+    assert f"messages.show" in endpoints
+    assert f"messages.new" in endpoints
+    assert f"messages.create" in endpoints
+    assert f"messages.edit" in endpoints
+    assert f"messages.update" in endpoints
+    assert f"messages.delete" in endpoints
