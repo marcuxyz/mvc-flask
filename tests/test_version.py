206538710383fb296e921ef657317238ec743638
Marcus Pereira <mpereirassa@gmail.com>
2023-11-12 14:39:08 -0300

    chore(tests): remove outdated version test file test(version_test.py): update version test to use the correct version number

--- tests/test_version.py
+++
@@ -1,7 +0,0 @@
-from mvc_flask.__version__ import __version__
-from ward import test, expect
-
-
-@test("check version")
-def _():
-    expect.assert_equal(__version__, "2.5.0", None)
--- tests/version_test.py
+++ tests/version_test.py
@@ -1,8 +1,4 @@
-from unittest import TestCase
-
 from mvc_flask.__version__ import __version__
 
-
-class VersionTest(TestCase):
-    def test_version(self):
-        self.assertEqual(__version__, "2.7.0")
+def test_mvc_flask_currently_version():
+    __version__ == "2.7.0"
