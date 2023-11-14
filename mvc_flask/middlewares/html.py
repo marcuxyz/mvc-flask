54f79997b0e3c7dc922d3a3b1d04818c916a9a00
Marcus Pereira <mpereirassa@gmail.com>
2023-11-14 14:47:00 -0300

    refactor(html.py): improve code readability and add docstrings for HTMLMiddleware class and its methods feat(html.py): add method to handle the generation of appropriate HTML based on a given string

--- mvc_flask/middlewares/html.py
+++ mvc_flask/middlewares/html.py
@@ -2,16 +2,58 @@ import markupsafe
 
 
 class HTMLMiddleware:
+    """
+    A middleware class for handling HTML-related operations, specifically for creating hidden input fields
+    with specific methods (like PUT and DELETE) that are not natively supported by HTML forms.
+
+    Methods:
+    - _input_html: Private method to generate HTML input element.
+    - _put: Private method to generate a hidden input field for the PUT method.
+    - _delete: Private method to generate a hidden input field for the DELETE method.
+    - method: Public method to handle the generation of appropriate HTML based on a given string.
+    """
+
     def _input_html(self, input_method):
+        """
+        Generates a hidden HTML input element.
+
+        Args:
+        - input_method (str): The HTTP method to be used (e.g., 'put', 'delete').
+
+        Returns:
+        - str: An HTML string for a hidden input element with the specified method.
+        """
         return f"<input type='hidden' name='_method' value={input_method.upper()}>"
 
     def _put(self):
+        """
+        Generates a hidden input field for the PUT method.
+
+        Returns:
+        - str: An HTML string for a hidden input element for the PUT method.
+        """
         return self._input_html("put")
 
     def _delete(self):
+        """
+        Generates a hidden input field for the DELETE method.
+
+        Returns:
+        - str: An HTML string for a hidden input element for the DELETE method.
+        """
         return self._input_html("delete")
 
     def method(self, string):
+        """
+        Determines the appropriate HTML string to return based on the given method string.
+
+        Args:
+        - string (str): The method string (e.g., 'put', 'delete').
+
+        Returns:
+        - Markup: A markupsafe.Markup object containing the appropriate HTML string.
+                  This object is safe to render directly in templates.
+        """
         result = {
             "put": self._put(),
             "delete": self._delete(),
