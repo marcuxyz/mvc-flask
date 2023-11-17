ed810047f2cba825727282d347a615c7c9845aaa
Marcus Pereira <mpereirassa@gmail.com>
2023-11-17 12:25:43 -0300

    refactor(mvc_flask): reorganize code structure and improve readability
    - Move FlaskMVC class to a separate file `mvc_flask.py` for better organization
    - Remove unnecessary imports and unused code from `__init__.py`
    - Rename `init_app` method in `FlaskMVC` class to `perform` for better clarity
    - Extract configuration logic into separate methods in `FlaskMVC` class for better modularity and readability
    - Update method names in `FlaskMVC` class to better reflect their purpose
    - Update variable names in `FlaskMVC` class for better clarity
    - Update comments and docstrings in `FlaskMVC` class for better understanding

--- mvc_flask/__init__.py
+++ mvc_flask/__init__.py
@@ -1,31 +1,3 @@
-from flask import Flask
+from .mvc_flask import FlaskMVC, Router
 
-from .middlewares.http.router_middleware import RouterMiddleware as Router
-
-from .middlewares.http.method_override_middleware import MethodOverrideMiddleware
-from .middlewares.http.custom_request_middleware import CustomRequestMiddleware
-from .middlewares.blueprint_middleware import BlueprintMiddleware
-
-
-from .helpers.html.input_method_helper import InputMethodHelper
-
-
-class FlaskMVC:
-    def __init__(self, app: Flask = None, path="app"):
-        if app is not None:
-            self.init_app(app, path)
-
-    def init_app(self, app: Flask = None, path="app"):
-        self.path = path
-
-        app.template_folder = "views"
-        app.request_class = CustomRequestMiddleware
-        app.wsgi_app = MethodOverrideMiddleware(app.wsgi_app)
-
-        # register blueprint
-        # register_blueprint(app)
-        BlueprintMiddleware(app, path).register()
-
-        @app.context_processor
-        def inject_stage_and_region():
-            return dict(method=InputMethodHelper().input_hidden_method)
+__all__ = ["FlaskMVC", "Router"]
---
+++ mvc_flask/mvc_flask.py
@@ -0,0 +1,45 @@
+from flask import Flask
+
+from .middlewares.http.router_middleware import RouterMiddleware as Router
+
+from .middlewares.http.method_override_middleware import MethodOverrideMiddleware
+from .middlewares.http.custom_request_middleware import CustomRequestMiddleware
+from .middlewares.blueprint_middleware import BlueprintMiddleware
+
+
+from .helpers.html.input_method_helper import InputMethodHelper
+
+
+class FlaskMVC:
+    def __init__(self, app: Flask = None, path="app"):
+        if app is not None:
+            self.init_app(app, path)
+
+    def init_app(self, app: Flask = None, path="app"):
+        self.perform(app, path)
+
+    def perform(self, app: Flask, path: str):
+        self._configure_template_folder(app)
+        self._configure_custom_request_middleware(app)
+        self._configure_method_override_middleware(app)
+        self._configure_blueprint_middleware(app, path)
+        self._inject_object_in_jinja_template(app)
+
+    def _configure_template_folder(self, app):
+        app.template_folder = "views"
+
+    def _configure_custom_request_middleware(self, app):
+        app.request_class = CustomRequestMiddleware
+
+    def _configure_method_override_middleware(self, app):
+        app.wsgi_app = MethodOverrideMiddleware(app.wsgi_app)
+
+    def _configure_blueprint_middleware(self, app, path):
+        BlueprintMiddleware(app, path).register()
+
+    def _inject_object_in_jinja_template(self, app):
+        @app.context_processor
+        def inject_stage_and_region():
+            return {
+                "method": InputMethodHelper().input_hidden_method,
+            }
