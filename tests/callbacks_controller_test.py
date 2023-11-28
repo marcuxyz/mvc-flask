ae8083f3e718d4e87f5bb40f28cecc195bdb261e
Marcus Pereira <mpereirassa@gmail.com>
2023-11-28 16:42:23 -0300

    feat(callback_middleware.py): add register method to CallbackMiddleware class to register before_request and after_request hooks

--- tests/callbacks_controller_test.py
+++ tests/callbacks_controller_test.py
@@ -1,7 +1,14 @@
 from flask import url_for
 
 
-def test_create_a_message(client):
+def test_before_request(client):
     response = client.get(url_for("callbacks.index"))
 
-    assert response.text == "index"
+    assert response.text == "before request message"
+
+
+def test_after_request(client):
+    response = client.get(url_for("callbacks.show", id=1))
+
+    assert response.headers["from_after_request"] == "yes"
+    assert response.text == "hello"
